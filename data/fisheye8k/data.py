0fe968062e78b35e14f8912e9f23a1021dcf060c
nnn27072005 <ngu.nguyen2775@hcmut.edu.vn>
2026-03-29 22:18:05 +0700

    init

--- data/fisheye8k/data.py
+++
@@ -1,4 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="XwtWeFEZTiyYITifFwXW")  # free account
-project = rf.workspace("ai-city-challenge").project("fisheye8k")
-dataset = project.version(1).download("yolov8")  # compatible với ultralytics
